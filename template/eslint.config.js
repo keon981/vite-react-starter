5f31dcd0ce5d89bbfd31cac4a7876f49ed1a1152
Keon.ke <Keon.ke@everfortuneai.com.tw>
2025-06-20 11:07:30 +0800

    🦄 refactor: fix eslint and style

--- template/eslint.config.js
+++ template/eslint.config.js
@@ -1,7 +1,9 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  stylistic: true,
+  stylistic: {
+    braceStyle: '1tbs',
+  },
 
   // TypeScript and Vue are autodetected, you can also explicitly enable them:
   typescript: true,
@@ -24,39 +26,50 @@ export default antfu({
     'style/jsx-one-expression-per-line': 'off',
     'style/jsx-curly-spacing': 'off',
     'style/jsx-child-element-spacing': 'off',
+
     'brace-style': ['error', '1tbs'],
+    'style/brace-style': ['error', '1tbs'],
     'no-console': 'warn',
     'no-nested-ternary': 'error',
     'antfu/if-newline': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'perfectionist/sort-imports': ['error', {
+      customGroups: [
+        {
+          groupName: 'react',
+          elementNamePattern: '^react$',
+        },
+        {
+          groupName: 'react-related',
+          elementNamePattern: '^react-',
+        },
+        {
+          groupName: 'tanstack-react',
+          elementNamePattern: '^@tanstack/react-',
+        },
+      ],
+      groups: [
+        // 將 React 相關模組作為單獨的群組放在最前面，react 優先
+        'react',
+        ['react-related', 'tanstack-react'],
+        'external',
+        'builtin',
+        'internal',
+        ['parent', 'sibling', 'index'],
+        'type',
+        'side-effect',
+        'unknown',
+      ],
+      newlinesBetween: 'always',
+      order: 'asc',
+      type: 'natural',
+    }],
+  },
+}, {
+  // 針對生成的檔案禁用特定規則
+  files: ['**/*.gen.ts', '**/*.gen.tsx'],
+  rules: {
+    'eslint-comments/no-unlimited-disable': 'off',
+    'eslint-comments/no-unused-disable': 'off',
   },
 })
-  .overrides({
-    'antfu/imports/rules': {
-      rules: {
-        'import/order': [
-          'error',
-          {
-            'groups': ['builtin', 'external', 'internal'],
-            'pathGroups': [
-              {
-                pattern: 'react',
-                group: 'external',
-                position: 'before',
-              },
-            ],
-            'pathGroupsExcludedImportTypes': ['react'],
-            'newlines-between': 'always',
-            'alphabetize': {
-              order: 'asc',
-              caseInsensitive: true,
-            },
-          },
-        ],
-      },
-    },
-    'antfu/perfectionist/setup': {
-      rules: {
-        'perfectionist/sort-imports': 'off',
-      },
-    },
-  })
